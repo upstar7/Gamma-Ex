4aeddbe50fde79202dea10504eeeeb58a8fc763f
Dusan <backend@greca.co>
2023-07-28 09:34:49 -0400

    build is done

--- gamma.py
+++ gamma.py
@@ -15,7 +15,10 @@ import undetected_chromedriver as uc
 import time
 import random
 import os
-import tkinter as tk
+from tkinter import Tk, Label, Button, OptionMenu, StringVar, ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
+
 
 # filePath = os.path.expanduser('~/Downloads/cvna_quotedata.csv')
 
@@ -77,6 +80,19 @@ import tkinter as tk
 
 # try:
 #     delay(1, 3)
+#     optionRangeAll = driver.find_element(By.CSS_SELECTOR, '#root > div > div > div.Box-sc-jzm6b1-0.cvqwTQ > div.Box-sc-jzm6b1-0.Tabs___StyledBox-sc-zx70g3-6.jkMlqj > div:nth-child(2) > div.Box-sc-jzm6b1-0.Flex-sc-9zr7dk-0.cXQocl > div:nth-child(4) > div > div.Box-sc-jzm6b1-0.FormField__Field-sc-150d642-0.dTWjar > div > div > div')
+#     optionRangeAll.click()
+# except:
+#     pass
+# try:
+#     delay(1, 3)
+#     optionRangeAll = driver.find_element(By.CSS_SELECTOR, '.ReactSelect__menu > div > div:nth-child(1)')
+#     optionRangeAll.click()
+# except:
+#     pass
+
+# try:
+#     delay(1, 3)
 #     viewChainButton = driver.find_element(By.CSS_SELECTOR, '.Button__StyledButton-sc-1tdgwi0-2.gNcAGf')
 #     viewChainButton.click()
 # except:
@@ -89,28 +105,127 @@ import tkinter as tk
 # except:
 #     pass
 
-
 pd.options.display.float_format = '{:,.4f}'.format
 
-# Black-Scholes European-Options Gamma
 
-def calcGammaEx(S, K, vol, T, r, q, optType, OI):
-    if T == 0 or vol == 0:
-        return 0
+#Create the main Tkinter application
+root = Tk()
+root.title("Vanna/Gamma Exposure Visualization")
+root.geometry("1400x700")
+root.configure(bg="black")
 
-    dp = (np.log(S/K) + (r - q + 0.5*vol**2)*T) / (vol*np.sqrt(T))
-    dm = dp - vol*np.sqrt(T)
+# Define fig outside of the calculateAndDisplay() function
+fig = None
 
-    if optType == 'call':
-        gamma = np.exp(-q*T) * norm.pdf(dp) / (S * vol * np.sqrt(T))
-        return OI * 100 * S * S * 0.01 * gamma
-    else:  # Gamma is same for calls and puts. This is just to cross-check
-        gamma = K * np.exp(-r*T) * norm.pdf(dm) / (S * S * vol * np.sqrt(T))
-        return OI * 100 * S * S * 0.01 * gamma
+def calculateAndDisplay():
+    
+    global fig
+    
+    selectedDate = datetime.strptime(selectedDateVar.get(), '%Y-%m-%d')
+    
+    # Filter data based on selected expiration date
+    filteredDf = df[df['ExpirationDate'] == selectedDate]
+    dfAgg = filteredDf.groupby(['StrikePrice'])[['Vanna', 'CallGEX', 'PutGEX', 'TotalGamma']].sum()
+    strikes = dfAgg.index.values
+    
+    # Create three subplots within one figure
+    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    fig.suptitle("Vanna/Gamma Exposure Visualization", fontsize=16)
+    fig.set_facecolor('black')
+    
+    # Add the selected expiration date as a text annotation
+    expirationDateText = selectedDate.strftime('%Y-%m-%d')
+    fig.text(0.5, 0.98, f"Expiration Date: {expirationDateText}", ha='center', va='top', color='yellow', fontsize=16)
+
+    # Call and Put Gamma Display
+    xnew = np.linspace(strikes.min(), strikes.max(), 500)  # Smooth x values for interpolation
+    spl = make_interp_spline(strikes, dfAgg['Vanna'].to_numpy(), k=5)
+    ynew = spl(xnew)
+    axs[0].plot(xnew, ynew, linewidth=1.5, label="Total Vanna", color='green')    #Plot Chart
+    axs[0].set_title("Vanna", color='white')
+    axs[0].set_xlabel('Strike', color='white')
+    axs[0].set_ylabel('Total Vanna, $', color='white')
+    axs[0].set_facecolor('black')
+    axs[0].axhline(y=0, color='white', lw=0.5)
+    axs[0].grid(True, linestyle='dashed', lw=0.3)
+    axs[0].set_xlim([fromStrike, toStrike])
+    axs[0].yaxis.set_major_formatter(ticker.FuncFormatter(yAxisFormatter))
+    axs[0].tick_params(axis='x', colors='white')
+    axs[0].tick_params(axis='y', colors='white')
+    for spine in axs[0].spines.values():
+        spine.set_edgecolor('white')    
+
+    # Call and Put Gamma Display
+    axs[1].bar(strikes, dfAgg['CallGEX'].to_numpy(), width=0.2, linewidth=0.1, label="Call Gamma", color='green')
+    axs[1].bar(strikes, dfAgg['PutGEX'].to_numpy(), width=0.2, linewidth=0.1, label="Put Gamma", color='red')
+    axs[1].set_title("Gamma", color='white')
+    axs[1].set_xlabel('Strike', color='white')
+    axs[1].set_ylabel('Call and Put Gamma, $', color='white')
+    axs[1].set_facecolor('black')
+    axs[1].axhline(y=0, color='white', lw=0.5)
+    axs[1].grid(True, linestyle='dashed', lw=0.3)
+    axs[1].set_xlim([fromStrike, toStrike])
+    axs[1].yaxis.set_major_formatter(ticker.FuncFormatter(yAxisFormatter))
+    axs[1].legend(loc='upper left')
+    axs[1].tick_params(axis='x', colors='white')
+    axs[1].tick_params(axis='y', colors='white')
+    for spine in axs[1].spines.values():
+        spine.set_edgecolor('white')
+        
+    # Call and Put Gamma Display
+    chartTitle = "Total Gamma: $" + str("{:.2f}".format(df['TotalGamma'].sum() / 10 ** 6)) + "M / 1% CVNA MOVE"
+    xnew = np.linspace(strikes.min(), strikes.max(), 500)  # Smooth x values for interpolation
+    spl = make_interp_spline(strikes, dfAgg['TotalGamma'].to_numpy(), k=5)
+    ynew = spl(xnew)
+    axs[2].plot(xnew, ynew, linewidth=1.5, label="Call Gamma", color='green')    #Plot Chart
+    axs[2].set_title(chartTitle, color='white')
+    axs[2].set_xlabel('Strike', color='white')
+    axs[2].set_ylabel('Total Gamma Exposure, $', color='white')
+    axs[2].set_facecolor('black')
+    axs[2].axhline(y=0, color='white', lw=0.5)
+    axs[2].grid(True, linestyle='dashed', lw=0.3)
+    axs[2].set_xlim([fromStrike, toStrike])
+    axs[2].yaxis.set_major_formatter(ticker.FuncFormatter(yAxisFormatter))
+    axs[2].tick_params(axis='x', colors='white')
+    axs[2].tick_params(axis='y', colors='white')
+    for spine in axs[2].spines.values():
+        spine.set_edgecolor('white')    
+
+          
+    plt.tight_layout()
+    # plt.show()
+    
+    # Clear the plot_frame before displaying the new plots
+    for widget in plotFrame.winfo_children():
+        widget.destroy()
+
+    # Embed the plots in the plot_frame using FigureCanvasTkAgg
+    canvas = FigureCanvasTkAgg(fig, master=plotFrame)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    
+    # Add the NavigationToolbar2Tk to the plot_frame
+    toolbar = NavigationToolbar2Tk(canvas, plotFrame)
+    toolbar.update()
+    canvas.get_tk_widget().pack(side="bottom", fill="both", expand=True)
+      
+    # Calculate max and min values for each plot
+    maxVanna = dfAgg['Vanna'].max()
+    minVanna = dfAgg['Vanna'].min()
+    maxGamma = dfAgg['CallGEX'].max()
+    minGamma = dfAgg['PutGEX'].min()
+    maxGammaExposure = dfAgg['TotalGamma'].max()
+    minGammaExposure = dfAgg['TotalGamma'].min()
+    
+    # Add text annotations for max and min values to each plot
+    axs[0].text(0.98, 0.95, f"Max: {maxVanna:.2f}", ha='right', va='top', transform=axs[0].transAxes, color='green')
+    axs[0].text(0.98, 0.90, f"Min: {minVanna:.2f}", ha='right', va='top', transform=axs[0].transAxes, color='red')
+    axs[1].text(0.98, 0.95, f"Max: {maxGamma:.2f}", ha='right', va='top', transform=axs[1].transAxes, color='green')
+    axs[1].text(0.98, 0.90, f"Min: {minGamma:.2f}", ha='right', va='top', transform=axs[1].transAxes, color='red')
+    axs[2].text(0.98, 0.95, f"Max: {maxGammaExposure:.2f}", ha='right', va='top', transform=axs[2].transAxes, color='green')
+    axs[2].text(0.98, 0.90, f"Min: {minGammaExposure:.2f}", ha='right', va='top', transform=axs[2].transAxes, color='red')
 
 
-def isThirdFriday(d):
-    return d.weekday() == 4 and 15 <= d.day <= 21
 
 # Use forward slashes in the file path
 fileName = 'cvna_quotedata.csv'
@@ -133,7 +248,6 @@ toStrike = 1.2 * spotPrice
 dateLine = optionsFileData[2]
 todayDate = dateLine.split('Date: ')[1].split(',')
 monthDay = todayDate[0].split(' ')
-
 # Handling of US/EU date formats
 if len(monthDay) == 2:
     year = int(todayDate[1].split()[0])
@@ -154,7 +268,7 @@ df.columns = ['ExpirationDate', 'Calls', 'CallLastSale', 'CallNet', 'CallBid', '
               'PutNet', 'PutBid', 'PutAsk', 'PutVol', 'PutIV', 'PutDelta', 'PutGamma', 'PutOpenInt']
 
 df['ExpirationDate'] = pd.to_datetime(df['ExpirationDate'], format='%a %b %d %Y')
-df['ExpirationDate'] = df['ExpirationDate'] + timedelta(hours=16)
+# df['ExpirationDate'] = df['ExpirationDate'] + timedelta(hours=16)
 df['StrikePrice'] = df['StrikePrice'].astype(float)
 df['CallIV'] = df['CallIV'].astype(float)
 df['PutIV'] = df['PutIV'].astype(float)
@@ -164,32 +278,16 @@ df['CallOpenInt'] = df['CallOpenInt'].astype(float)
 df['PutOpenInt'] = df['PutOpenInt'].astype(float)
 
 # ---=== CALCULATE SPOT GAMMA ===---
+
+
 # Gamma Exposure = Unit Gamma * Open Interest * Contract Size * Spot Price
 # To further convert into 'per 1% move' quantity, multiply by 1% of spotPrice
 df['CallGEX'] = df['CallGamma'] * df['CallOpenInt'] * 100 * spotPrice * spotPrice * 0.01
 df['PutGEX'] = df['PutGamma'] * df['PutOpenInt'] * 100 * spotPrice * spotPrice * 0.01 * -1
 df['TotalGamma'] = (df.CallGEX + df.PutGEX) 
-dfAgg = df.groupby(['StrikePrice'])[['CallGEX', 'PutGEX', 'TotalGamma']].sum()
-strikes = dfAgg.index.values
-
-# Display Gamma Exposure
-fig = plt.figure()
-fig.set_facecolor('black')
-colors = ['green' if value >= 0 else 'red' for value in dfAgg['TotalGamma'].to_numpy()]
-chartTitle = "Total Gamma: $" + str("{:.2f}".format(df['TotalGamma'].sum() / 10 ** 6)) + "M per 1% CVNA Move"
-plt.rcParams['ytick.color'] = 'white'
-plt.rcParams['xtick.color'] = 'white'
-plt.rcParams['axes.edgecolor'] = 'white'
-plt.rcParams['axes.edgecolor'] = 'white'
-plt.grid(True, linestyle='dashed', lw=0.3)
-plt.bar(strikes, dfAgg['TotalGamma'].to_numpy(), width=0.2, linewidth=0.1, label="Gamma Exposure", color=colors)
-plt.gca().set_facecolor('black')
-plt.xlim([fromStrike, toStrike])
-plt.title(chartTitle, fontweight="bold", fontsize=20, color='white')
-plt.xlabel('Strike', fontweight="bold", color='white')
-plt.ylabel('Spot Gamma Exposure ($ / 1% move)', fontweight="bold", color='white')
-plt.axhline(y=0, color='white', lw=0.5)
-plt.legend()
+df['VannaWeight'] = abs(df['TotalGamma']) / 10 ** 6
+df['Days'] = (df['ExpirationDate'] - todayDate).dt.days
+df['Vanna'] = df['TotalGamma'] * df['Days'] * df['VannaWeight']
 
 # Define custom formatter function
 def yAxisFormatter(y, pos):
@@ -198,6 +296,23 @@ def yAxisFormatter(y, pos):
     else:
         return f"{y/1000000:.1f}M"
 
-# Apply formatter to the y-axis tick labels
-plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(yAxisFormatter))
-plt.show()
+plotFrame = ttk.Frame(root)
+plotFrame.pack(side='top', fill='both', expand=True, anchor='nw')
+
+# Create a frame to hold the OptionMenu and View button
+optionsFrame = ttk.Frame(root)
+optionsFrame.place(x=250, y=4)
+
+# UI components
+selectedDateVar = StringVar(root)
+selectedDateVar.set(df['ExpirationDate'].min().strftime('%Y-%m-%d'))
+dateSelectBox = OptionMenu(optionsFrame, selectedDateVar, * df['ExpirationDate'].dt.strftime('%Y-%m-%d').unique())
+dateSelectBox.config(font=('Helvetica', 13), width=12, bg='lightgrey')
+dateSelectBox.pack(side='left', padx=10)
+
+viewButton = Button(optionsFrame, text="View", command=calculateAndDisplay, fg='blue', bg='greenyellow', activebackground='greenyellow', activeforeground='blue', width=10, font=('Helvetica', 13, "bold"))
+viewButton.pack(side='left', padx=10)
+
+calculateAndDisplay()
+
+root.mainloop()
